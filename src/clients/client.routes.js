b97b3db56b0c74cebcf68c1c326a082e58b03b13
afigueroa-2023106 <afigueroa-2023106@kinal.edu.gt>
2025-04-28 22:09:01 -0600

    Se agrega la entidad clients con su controller y su routes

---
+++ src/clients/client.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express"
+import {
+    getAllClients,
+    getClientById,
+    createClient,
+    updateClient,
+    deleteClient
+} from "./client.controller.js"
+import { validateJwt } from "../../middlewares/validate.jwt.js"
+
+const router = Router()
+
+router.get('/', [validateJwt], getAllClients)
+router.get('/:id', [validateJwt], getClientById)
+router.post('/', [validateJwt], createClient)
+router.put('/:id', [validateJwt], updateClient)
+router.delete('/:id', [validateJwt], deleteClient)
+
+export default router;
